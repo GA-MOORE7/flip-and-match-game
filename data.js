baf0567773355ca8b7fdf8b9450ffaaea7424346
Gerald Arthur Moore <gerald.moore42@gmail.com>
2025-11-02 07:13:23 +0200

    modularise data flow

--- data.js
+++ data.js
@@ -19,17 +19,7 @@ const data = {
   ]
 };
 
-// Convert MongoDB-like object to pairs array
-function convertToPairs(dataObj) {
-  if (!dataObj || !dataObj.items) return [];
-  return dataObj.items.map(item => ({
-    id: item._id,
-    picture: item.imageUrl,
-    word: item.word
-  }));
-}
-
-export const pairs = convertToPairs(data);
+export { data };
 
 
 
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-import { pairs } from './data.js';
+import { data } from './data.js';
+import { convertToPairs } from './transformData.js';
 import { getPairs } from './getPairs.js';
 import { shuffle } from './shuffle.js';
 import { generateGrid } from './grid.js';
 import { attachClickHandler } from './attachClickHandler.js';
 import { checkMatch } from './checkMatch.js';
+
+const pairs = convertToPairs(data);
 
 const cards = getPairs(pairs);
 
